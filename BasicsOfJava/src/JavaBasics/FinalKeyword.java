ad4589e16a2a8d3c34ce493dea2040f719b9e9d9
Vishthangaraj <42928940+vishthanga@users.noreply.github.com>
2018-09-12 16:31:45 +0530

    Add files via upload

---
+++ BasicsOfJava/src/JavaBasics/FinalKeyword.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+
+		final int wheels = 4;
+		
+		final int i = 10;
+		//i = 20;
+	//i = 30;
+		
+		System.out.println(i + "  " +"   "+ wheels);
+		
+		
+	}
+
+}
